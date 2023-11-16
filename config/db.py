1f2933ab37a8049a05881eca31c8c6640149e832
Annuar Abouharb <142844802+Annuar2203@users.noreply.github.com>
2023-11-16 17:33:20 -0400

    Update db.py with generals specifications

--- config/db.py
+++ config/db.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:22032004@localhost:3306/databasepython")
+engine = create_engine("mysql+pymysql://name:password@localhost:port/name_database")
 
+# Para realizar la conexion de manera correcta se hace lo siguiente
+# usuario: se coloca el usuario en donde se encuentra la base de datos, MySQL lo suele colocar de manera determinada como root
+# password: La contraseña del usuario en donde se encuentra la base de datos
+# Port: El puerto donde se encuentra el usuario
+# name_database: El nombre de la base de datos
 
 
 metadata = MetaData()
